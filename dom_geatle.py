33b55a526139c045cfe742f716049f1529a0e65f
jordandavis16 <64336652+jordandavis16@users.noreply.github.com>
2023-04-27 04:28:48 -0400

    Update dom_geatle.py

--- dom_geatle.py
+++ dom_geatle.py
@@ -34,13 +34,13 @@ script_template = PromptTemplate(
 )
 
 # Memory 
-title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
-script_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
+# title_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
+script_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
 
 
 # Llms
 llm = OpenAI(temperature=0.9) 
-title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=title_memory)
+# title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=title_memory)
 script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script', memory=script_memory)
 
 wiki = WikipediaAPIWrapper()
